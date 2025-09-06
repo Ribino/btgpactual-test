6a0483c683ac18c33e5251f732535023b5ecf3cf
Ribino <lribino@outlook.com>
2025-09-06 16:01:57 -0300

    chore: inital commit with first system setup

---
+++ order-processor/src/main/java/com/btg/challenge/order_processor/config/RabbitMQConfig.java
@@ -0,0 +1,10 @@
+package com.btg.challenge.order_processor.config;
+
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+}
---
+++ order-processor/src/test/java/com/btg/challenge/order_processor/OrderProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.btg.challenge.order_processor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
